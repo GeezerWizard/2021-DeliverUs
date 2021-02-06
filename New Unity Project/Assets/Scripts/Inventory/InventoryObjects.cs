fd9b47e519f24d1b08321eed576ae6fb1d47bc10
David Dam <damnation99@hotmail.com>
2021-02-06 12:53:11 +1100

    Init
    Initialise repository

---
+++ New Unity Project/Assets/Scripts/Inventory/InventoryObjects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewInventoryObject", menuName = "Object")]
+public class InventoryObjects : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public int amount;
+    public int dollarydoos;
+
+    public Sprite artwork;
+}
