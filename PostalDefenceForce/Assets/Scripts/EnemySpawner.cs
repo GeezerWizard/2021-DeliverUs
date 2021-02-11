ad155d177a43fbbd88523d4cd152e634effda5fe
David Dam <damnation99@hotmail.com>
2021-02-11 18:15:58 +1100

    Commit before gitignore refresh

---
+++ PostalDefenceForce/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyToSpawn;
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(EnemyToSpawn, transform.position, Quaternion.identity);
+    }
+}
---
+++ PostalDefenceForce/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 resetPosition = Vector3.zero;
+    void Start()
+    {
+        transform.position = resetPosition;
+        print(transform.gameObject + " position reset to " + resetPosition);
+    }
+}
